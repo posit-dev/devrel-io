b9038f8c233b1fb85b6435a034932d10a29c3783
Jeroen Janssens <jeroen@jeroenjanssens.com>
2026-02-05 13:58:11 +0100

    Add DWH data source for Positron metrics

--- scripts/output-to-parquet.py
+++ scripts/output-to-parquet.py
@@ -8,7 +8,7 @@ dfs = [
     pl.read_ndjson(f"data/output/{x}/*/*.jsonl", include_file_paths="source")
     .with_columns(pl.col("date").str.to_date())
     .unique(["project_id", "metric", "date", "source"])
-    for x in ["cran", "openvsx", "plausible", "pypi"]
+    for x in ["cran", "openvsx", "plausible", "pypi", "dwh"]
 ]
 
 dfs.append(
